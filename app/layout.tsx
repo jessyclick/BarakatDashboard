21d0405be76d540240ee80ce7917c08e191a8f54
ykylassov <ikilasoff@gmail.com>
2026-01-15 20:31:18 +0500

    Add plus icon tn

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Barakat Dashboard",
+  description: "The Barakat Dashboard",
 };
 
 export default function RootLayout({
